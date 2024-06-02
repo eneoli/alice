a06761dcaa420b2e3b7cd0537a21dd543d6c66eb
eneoli <oliver@eneoli.de>
2024-06-02 23:48:39 +0200

    made compatible with recent changes

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,45 @@
+use std::collections::HashMap;
+
 use chumsky::Parser;
-use server::core::{parse::{fol::fol_parser, lexer::lexer}, prop::Prop};
+use server::core::{
+    check::identifier_context::IdentifierContext,
+    parse::{fol::fol_parser, lexer::lexer, proof::proof_parser},
+    process::{stages::resolve_datatypes::ResolveDatatypes, ProofPipeline},
+    proof::Proof,
+    typify,
+};
 
 fn main() {
-    let prop = Prop::Impl(
-        Box::new(Prop::And(
-            Box::new(Prop::And(
-                Box::new(Prop::Atom(String::from("A"), None)),
-                Box::new(Prop::Impl(
-                    Box::new(Prop::Atom(String::from("A"), None)),
-                    Box::new(Prop::Atom(String::from("B"), None)),
-                )),
-            )),
-            Box::new(Prop::Impl(
-                Box::new(Prop::Atom(String::from("B"), None)),
-                Box::new(Prop::Atom(String::from("C"), None)),
-            )),
-        )),
-        Box::new(Prop::Atom(String::from("C"), None)),
-    );
-
-    let prop2 = Prop::Impl(
-        Box::new(Prop::Atom(String::from("A"), None)),
-        Box::new(Prop::Atom(String::from("A"), None)),
-    );
-
     let src = std::fs::read_to_string("test.proof").unwrap();
 
+    // Step 1: Parse tokens
     let tokens = lexer().parse(src.clone());
     println!("{:#?}", tokens);
 
-    let ast = fol_parser().parse(tokens.unwrap());
+    // Step 2: Parse Proof
+    let proof: Proof = proof_parser().parse(tokens.unwrap()).unwrap();
 
-    println!("{:#?}", ast);
+    // Step 3: Preprocess ProofTerm
 
-    // println!("{}", prove(prop));
+    let processed_proof = ProofPipeline::new()
+        .pipe(ResolveDatatypes::boxed())
+        .apply(proof);
+
+    println!("{:#?}", processed_proof);
+
+    let mut ctx = IdentifierContext::new();
+    let _type = typify(
+        &processed_proof.proof_term,
+        &processed_proof.datatypes,
+        &mut ctx,
+    );
+
+    println!("{:#?}", _type);
+
+    let t = lexer().parse("A & B(x, x) & \\forall x:t. A(x)").unwrap();
+    let mut p = fol_parser().parse(t).unwrap();
+
+    p.substitue_free_parameter(&"x".to_string(), &"y".to_string());
+
+    println!("{:#?}", p);
 }
