c84fea5b0c6e52e0a9cec713e450948416e4433b
eneoli <oliver@eneoli.de>
2024-06-05 19:12:05 +0200

    added ProofTerm visitor

--- src/core/proof_term.rs
+++ src/core/proof_term.rs
@@ -52,4 +52,67 @@ impl ProofTerm {
     pub fn boxed(&self) -> Box<Self> {
         Box::new(self.clone())
     }
+
+    pub fn visit<R>(&self, visitor: &mut impl ProofTermVisitor<R>) -> R {
+        match self {
+            ProofTerm::Ident(ident) => visitor.visit_ident(ident),
+            ProofTerm::Pair(fst, snd) => visitor.visit_pair(fst, snd),
+            ProofTerm::ProjectFst(body) => visitor.visit_project_fst(body),
+            ProofTerm::ProjectSnd(body) => visitor.visit_project_snd(body),
+            ProofTerm::Function {
+                param_ident,
+                param_type,
+                body,
+            } => visitor.visit_function(param_ident, param_type, body),
+            ProofTerm::Application {
+                function,
+                applicant,
+            } => visitor.visit_application(function, applicant),
+            ProofTerm::LetIn {
+                fst_ident,
+                snd_ident,
+                pair_proof_term,
+                body,
+            } => visitor.visit_let_in(fst_ident, snd_ident, pair_proof_term, body),
+            ProofTerm::OrLeft { body, other } => visitor.visit_or_left(body, other),
+            ProofTerm::OrRight { body, other } => visitor.visit_or_right(body, other),
+            ProofTerm::Case {
+                proof_term,
+                left_ident,
+                left_term,
+                right_ident,
+                right_term,
+            } => visitor.visit_case(proof_term, left_ident, left_term, right_ident, right_term),
+            ProofTerm::Abort(body) => visitor.visit_abort(body),
+            ProofTerm::Unit => visitor.visit_unit(),
+        }
+    }
+}
+
+pub trait ProofTermVisitor<R> {
+    fn visit_ident(&mut self, ident: &String) -> R;
+    fn visit_pair(&mut self, fst: &ProofTerm, snd: &ProofTerm) -> R;
+    fn visit_project_fst(&mut self, body: &ProofTerm) -> R;
+    fn visit_project_snd(&mut self, body: &ProofTerm) -> R;
+    fn visit_function(&mut self, param_ident: &String, param_type: &Type, body: &ProofTerm) -> R;
+    fn visit_application(&mut self, function: &ProofTerm, applicant: &ProofTerm) -> R;
+    fn visit_let_in(
+        &mut self,
+        fst_ident: &String,
+        snd_ident: &String,
+        pair_proof_term: &ProofTerm,
+        body: &ProofTerm,
+    ) -> R;
+    fn visit_or_left(&mut self, body: &ProofTerm, other: &Prop) -> R;
+    fn visit_or_right(&mut self, body: &ProofTerm, other: &Prop) -> R;
+    fn visit_case(
+        &mut self,
+        proof_term: &ProofTerm,
+        left_ident: &String,
+        left_term: &ProofTerm,
+        right_ident: &String,
+        right_term: &ProofTerm,
+    ) -> R;
+    fn visit_abort(&mut self, body: &ProofTerm) -> R;
+    fn visit_unit(&mut self) -> R;
 }
