02e555a181482e4195c40b91b53529a5a3f4383e
eneoli <oliver@eneoli.de>
2024-06-08 00:48:48 +0200

    added code editor

---
+++ frontend/src/domain/code-editor/components/code-editor.tsx
@@ -0,0 +1,141 @@
+import React from 'react';
+import { Editor, Monaco } from '@monaco-editor/react';
+import { useState } from 'react';
+
+interface CodeEditorProps {
+    onChange: (value: string) => void;
+}
+
+
+function handleEditorWillMount(monaco: Monaco) {
+    // here is the monaco instance
+    // do something before editor is mounted
+    monaco.languages.typescript.javascriptDefaults.setEagerModelSync(true);
+
+    const keywords = ['fn', 'case', 'of', 'let', 'in', 'datatype', 'inl', 'inr'];
+
+    monaco.languages.register({ id: 'alice' });
+    monaco.languages.setMonarchTokensProvider('alice', {
+        keywords,
+        tokenizer: {
+            root: [
+                [/@?[a-zA-Z][\w$]*/, {
+                    cases: {
+                        '@keywords': 'keyword',
+                        '@default': 'variable',
+                    }
+                }]
+            ]
+        }
+    });
+
+    monaco.languages.registerCompletionItemProvider('alice', {
+        provideCompletionItems: function (model, position) {
+            // Get the text before the cursor
+            const word = model.getWordUntilPosition(position);
+            const rangeBackslash = {
+                startLineNumber: position.lineNumber,
+                endLineNumber: position.lineNumber,
+                startColumn: word.startColumn - 1,
+                endColumn: word.endColumn,
+            };
+
+            const range = {
+                startLineNumber: position.lineNumber,
+                endLineNumber: position.lineNumber,
+                startColumn: word.startColumn,
+                endColumn: word.endColumn,
+            };
+
+            const keywordCompletions = keywords.map((keyword) => ({
+                documentation: keyword,
+                insertText: keyword,
+                kind: monaco.languages.CompletionItemKind.Keyword,
+                label: keyword,
+                range: range
+            }));
+
+            return {
+                suggestions: [
+                    {
+                        documentation: 'ForAll Quantor',
+                        insertText: '∀',
+                        kind: monaco.languages.CompletionItemKind.Snippet,
+                        label: '\\forall',
+                        range: rangeBackslash
+                    },
+                    {
+                        documentation: 'Exists Quantor',
+                        insertText: '∃',
+                        kind: monaco.languages.CompletionItemKind.Snippet,
+                        label: '\\exists',
+                        range: rangeBackslash
+                    },
+                    {
+                        documentation: 'Logical And',
+                        insertText: '∧',
+                        kind: monaco.languages.CompletionItemKind.Snippet,
+                        label: '\\and',
+                        range: rangeBackslash
+                    },
+                    {
+                        documentation: 'Logical Or',
+                        insertText: '∨',
+                        kind: monaco.languages.CompletionItemKind.Snippet,
+                        label: '\\or',
+                        range: rangeBackslash
+                    },
+                    {
+                        documentation: 'Logical Implies',
+                        insertText: '→',
+                        kind: monaco.languages.CompletionItemKind.Snippet,
+                        label: '\\implies',
+                        range: rangeBackslash
+                    },
+                    {
+                        documentation: 'Logical Not',
+                        insertText: '¬',
+                        kind: monaco.languages.CompletionItemKind.Snippet,
+                        label: '\\not',
+                        range: rangeBackslash
+
+                    },
+                    ...keywordCompletions
+                ],
+            };
+        },
+        triggerCharacters: ['\\']
+    });
+
+    monaco.editor.defineTheme('alice-theme', {
+        base: 'vs',
+        inherit: true,
+        colors: {},
+
+        rules: [
+            { token: 'keyword', foreground: '#006af5', fontStyle: 'bold' },
+        ]
+    });
+}
+
+export function CodeEditor({ onChange }: CodeEditorProps) {
+
+    const [value, setValue] = useState('// Write your proof here!\n\n');
+
+    const onValueChange = (value: string | undefined, ev: unknown) => {
+        setValue(value || '');
+        onChange(value || '')
+    };
+
+    return (
+        <Editor height="50vh"
+            beforeMount={handleEditorWillMount}
+            onChange={onValueChange}
+            value={value}
+            defaultLanguage='alice'
+            options={{
+                minimap: { enabled: false },
+            }}
+            theme='alice-theme' />
+    );
+}
