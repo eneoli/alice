3d8a24fd72bb1dc102a3c422bd40c72ec93bdae8
eneoli <oliver@eneoli.de>
2024-06-10 18:26:29 +0200

    added proof node component

--- frontend/src/domain/proof-tree/components/proof-node.tsx
+++ frontend/src/domain/proof-tree/components/proof-node.tsx
@@ -1,5 +1,5 @@
-import { css } from '@emotion/css';
-import React, { ReactNode } from 'react';
+import { css, cx } from '@emotion/css';
+import React, { ReactNode, useState } from 'react';
 import { ProofLine } from './proof-line';
 
 interface ProofNodeProps {
@@ -9,15 +9,20 @@ interface ProofNodeProps {
 }
 
 export function ProofNode({ children, content, label }: ProofNodeProps) {
-    return (
-        <div style={{width: '100%'}}>
 
-            {children && (<>
-                <div className={cssProofNode}>{children}</div>
-                <ProofLine label={label || ''} />
-            </>
+    const [padding, setPadding] = useState(0);
+
+    return (
+        <div>
+            {children && (
+                <>
+                    <div style={{paddingRight: padding + 5}} className={cssProofNode}>{children}</div>
+                    <ProofLine label={label || ''} onSizeChange={(size) => {
+                        setPadding(size);
+                    }} />
+                </>
             )}
-            <div className={cssProofNode}>
+            <div style={{paddingRight: padding}} className={cx(cssProofNode, cssProofNodeContent)}>
                 {content}
             </div>
         </div>
@@ -26,8 +31,15 @@ export function ProofNode({ children, content, label }: ProofNodeProps) {
 
 const cssProofNode = css`
     width: 100%;
-    display: flex;
-    flex: 1;
+    display: inline-flex;
     flex-direction: row;
+    gap: 25px;
     justify-content: space-evenly;
+    align-items: flex-end;
+    box-sizing: border-box;
+`;
+
+const cssProofNodeContent = css`
+    min-width: max-content;
+    box-sizing: border-box;
 `;
