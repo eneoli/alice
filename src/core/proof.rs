0d1a9b8cef179aa4b2f33cd1d1597c6bbeda666c
eneoli <oliver@eneoli.de>
2024-06-11 21:59:44 +0200

    added Serialize, Deserialize

--- src/core/proof.rs
+++ src/core/proof.rs
@@ -1,6 +1,8 @@
+use serde::{Deserialize, Serialize};
+
 use super::proof_term::ProofTerm;
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
 pub struct Proof {
     pub datatypes: Vec<String>,
     pub proof_term: ProofTerm,
