ecbdbf0a8c3a0f0637e55d3de69f57ece0a9bc5f
eneoli <oliver@eneoli.de>
2024-06-15 00:53:11 +0200

    implemented Display trait

--- src/kernel/parse/mod.rs
+++ src/kernel/parse/mod.rs
@@ -1,5 +1,7 @@
-pub mod lexer;
+use core::fmt;
+
 pub mod fol;
+pub mod lexer;
 pub mod proof;
 pub mod proof_term;
 
@@ -31,4 +33,37 @@ pub enum Token {
     IN,
     EQUAL,
     DATATYPE,
+}
+
+impl fmt::Display for Token {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Token::IDENT(s) => write!(f, "{}", s),
+            Token::AND => write!(f, "∧"),
+            Token::OR => write!(f, "∨"),
+            Token::ARROW => write!(f, "=>"),
+            Token::IMPLICATION => write!(f, "→"),
+            Token::NOT => write!(f, "¬"),
+            Token::LROUND => write!(f, "("),
+            Token::RROUND => write!(f, ")"),
+            Token::LANGLE => write!(f, "<"),
+            Token::RANGLE => write!(f, ">"),
+            Token::EXISTS => write!(f, "∃"),
+            Token::FORALL => write!(f, "∀"),
+            Token::DOT => write!(f, "."),
+            Token::COMMA => write!(f, ","),
+            Token::COLON => write!(f, ":"),
+            Token::SEMICOLON => write!(f, ";"),
+            Token::TRUE => write!(f, "⊤"),
+            Token::FALSE => write!(f, "⊥"),
+
+            Token::FN => write!(f, "fn"),
+            Token::CASE => write!(f, "case"),
+            Token::OF => write!(f, "of"),
+            Token::LET => write!(f, "let"),
+            Token::IN => write!(f, "in"),
+            Token::EQUAL => write!(f, "="),
+            Token::DATATYPE => write!(f, "datatype"),
+        }
+    }
 }
