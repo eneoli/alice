55b723d8bfe316163a99de95b365f28883c35c93
eneoli <oliver@eneoli.de>
2024-06-16 15:04:33 +0200

    added proper error handling

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,55 @@
-use chumsky::Parser;
 use alice::kernel::{
     check::typify,
     parse::{lexer::lexer, proof::proof_parser},
     process::{stages::resolve_datatypes::ResolveDatatypes, ProofPipeline},
-    proof::Proof,
 };
+use ariadne::{sources, Color, Label, Report, ReportKind, Source};
+use chumsky::{chain::Chain, error::Simple, Parser, Stream};
 
 fn main() {
     let src = std::fs::read_to_string("test.proof").unwrap();
 
     // Step 1: Parse tokens
     let tokens = lexer().parse(src.clone());
+
     println!("{:#?}", tokens);
 
-    // Step 2: Parse Proof
-    let proof: Proof = proof_parser().parse(tokens.unwrap()).unwrap();
+    if let Err(err) = tokens.clone() {
+        err.into_iter().for_each(|e| {
+            Report::build(ReportKind::Error, (), e.span().start)
+                .with_message(e.to_string())
+                .with_label(Label::new(e.span()).with_color(Color::Red))
+                .finish()
+                .print(Source::from(src.clone()))
+                .unwrap();
+        });
 
+        return;
+    }
+
+    // Step 2: Parse Proof
+    let len = src.chars().count();
+    let proof = proof_parser().parse(Stream::from_iter(len..len + 1, tokens.unwrap().into_iter()));
+
+    if let Err(err) = proof.clone() {
+        println!("{:#?}", err);
+
+        err.into_iter().for_each(|e| {
+            Report::build(ReportKind::Error, (), e.span().start)
+                .with_message(e.to_string())
+                .with_label(Label::new(e.span()).with_color(Color::Red))
+                .finish()
+                .print(Source::from(src.clone()))
+                .unwrap();
+        });
+
+        return;
+    }
     // Step 3: Preprocess ProofTerm
 
     let processed_proof = ProofPipeline::new()
         .pipe(ResolveDatatypes::boxed())
-        .apply(proof);
+        .apply(proof.unwrap());
 
     println!("{:#?}", processed_proof);
 
