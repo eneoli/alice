68e280a6d1df4522db019b62b85a62d28ce08af1
eneoli <oliver@eneoli.de>
2024-07-05 19:13:13 +0200

    added handleImplIntroRule

---
+++ frontend/src/domain/visual-proof-editor/proof-rule/proof-rule-handler/handle-impl-intro-rule.ts
@@ -0,0 +1,30 @@
+import { VisualProofEditorProofTree } from '../../components/visual-proof-editor';
+import { ProofRuleHandlerResult } from '../../components/visual-proof-editor-sidebar';
+import { generateIdentifier } from './identifier';
+import { createEmptyVisualProofEditorProofTree } from '../../../../util/create-visual-proof-editor-empty-proof-tree';
+
+export function handleImplIntroRule(proofTree: VisualProofEditorProofTree): ProofRuleHandlerResult {
+
+    const { conclusion } = proofTree;
+
+    if (proofTree.conclusion.kind != 'Impl') {
+        throw new Error('Conclusion is not an implication.');
+    }
+
+    const [fst, snd] = proofTree.conclusion.value;
+
+    return {
+        newProofTree: {
+            id: proofTree.id,
+            premisses: [createEmptyVisualProofEditorProofTree(snd)],
+            rule: 'ImplIntro',
+            conclusion,
+        },
+        additionalAssumptions: [{
+            kind: 'PropIsTrue',
+            prop: fst,
+            ident: generateIdentifier(),
+        }],
+    };
+
+}
