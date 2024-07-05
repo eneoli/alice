76c8ba52c43ee630716db4ecf829012ebc3c3e0b
eneoli <oliver@eneoli.de>
2024-07-05 19:04:21 +0200

    added VisualProofEditorAssumptionView

---
+++ frontend/src/domain/visual-proof-editor/components/visual-proof-editor-assumptiom-view.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { printProp } from '../../../util/print-prop';
+import { css } from '@emotion/css';
+import { Assumption } from './visual-proof-editor-sidebar';
+import { Button } from 'antd';
+
+interface VisualProofEditorAssumptionViewProps {
+    assumptions: Assumption[];
+    onAssumptionClick: (assumption: Assumption) => void;
+}
+
+export function VisualProofEditorAssumptionView(props: VisualProofEditorAssumptionViewProps) {
+    const { assumptions, onAssumptionClick } = props;
+
+    return (
+        <div>
+            <span>Assumptions: </span>
+            <ul className={cssAssumptionList}>
+                {
+                    assumptions.map((assumption, i) => (
+                        <li key={i} className={cssAssumptionListElement}>
+                            <Button onClick={() => onAssumptionClick(assumption)}>
+                                {displayAssumption(assumption)}
+                            </Button>
+                        </li>
+                    ))
+                }
+            </ul>
+        </div>
+    );
+}
+
+function displayAssumption(assumption: Assumption) {
+    switch (assumption.kind) {
+        case 'PropIsTrue': return assumption.ident + ': ' + printProp(assumption.prop);
+        case 'Datatype': return assumption.ident + ': ' + assumption.datatype;
+    }
+}
+
+const cssAssumptionList = css`
+    display: inline-block;
+    list-decoration: none;
+    padding: none;
+`;
+
+const cssAssumptionListElement = css`
+    display: inline;
+    margin-right: 5px;
+`;
