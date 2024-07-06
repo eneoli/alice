e18eedb4255539e81d3c2f0df109bf6a98d92011
eneoli <oliver@eneoli.de>
2024-07-06 15:14:16 +0200

    only show ui if valid prop entered

--- frontend/src/domain/app/components/app.tsx
+++ frontend/src/domain/app/components/app.tsx
@@ -3,25 +3,50 @@ import { Header } from './header';
 import { CodeEditor } from '../../code-editor/components/code-editor';
 import { VisualProofEditor } from '../../visual-proof-editor/components/visual-proof-editor';
 import { ConfigProvider, ThemeConfig } from 'antd';
+import { Prop, parse_prop } from 'alice';
+import { debounce, isEqual } from 'lodash';
 
 export function App() {
 
-    const [proofTerm, setProofTerm] = useState('');
+    const [_proofTerm, setProofTerm] = useState('');
     const [_analyzedProofTerm, setAnalyzedProofTerm] = useState('');
-    const [_prop, setProp] = useState('');
+    const [prop, setProp] = useState<Prop | null>(null);
+
+    const handlePropChange = debounce((propString: string) => {
+        try {
+            const newProp = parse_prop(propString);
+
+            if (!isEqual(prop, newProp)) {
+                setProp(newProp);
+            }
+
+            setAnalyzedProofTerm('');
+        } catch (e) {
+            setProp(null);
+            console.error(e);
+        }
+    }, 500);
 
     return (
         <ConfigProvider theme={theme}>
-            <Header onPropChange={() => setAnalyzedProofTerm('')} onVerify={(prop) => {
-                setProp(prop);
-                setAnalyzedProofTerm(proofTerm);
-            }} />
+            <Header onPropChange={handlePropChange} onVerify={(_prop) => { }} />
 
-            <VisualProofEditor />
+            {prop && (
+                <>
+                    <VisualProofEditor prop={prop} />
 
-            <div style={{ marginTop: 20 }}>
-                <CodeEditor onChange={setProofTerm} />
-            </div>
+                    <div style={{ marginTop: 20 }}>
+                        <CodeEditor height={'30vh'} onChange={setProofTerm} />
+                    </div>
+                </>
+            )}
+
+            {!prop && (
+                <div style={{ textAlign: 'center', color: '#192434' }}>
+                    <h1>Alice is ready.</h1>
+                    <h2>Please enter a proposition to begin.</h2>
+                </div>
+            )}
         </ConfigProvider>
     );
 }
