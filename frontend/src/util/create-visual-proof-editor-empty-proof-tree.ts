6d7060aababcd8c62365f7dd91e1c60e69ac837c
eneoli <oliver@eneoli.de>
2024-07-11 19:31:48 +0200

    conclusion as ProofTreeConclusion

--- frontend/src/util/create-visual-proof-editor-empty-proof-tree.ts
+++ frontend/src/util/create-visual-proof-editor-empty-proof-tree.ts
@@ -1,11 +1,12 @@
 import { Prop } from 'alice';
 import { v4 } from 'uuid';
+import { VisualProofEditorProofTree } from '../domain/visual-proof-editor/components/visual-proof-editor';
 
-export function createEmptyVisualProofEditorProofTree(conclusion: Prop) {
+export function createEmptyVisualProofEditorProofTreeFromProp(conclusion: Prop): VisualProofEditorProofTree {
     return {
         id: v4(),
         premisses: [],
         rule: null,
-        conclusion,
+        conclusion: { kind: 'PropIsTrue', value: conclusion },
     }
 }
