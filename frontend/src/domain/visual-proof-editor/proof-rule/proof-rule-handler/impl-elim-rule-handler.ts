78332bb97e73bf19d999170157fdd4eb29c29479
eneoli <oliver@eneoli.de>
2024-07-30 13:53:33 +0200

    added latex code, handle two premisses

--- frontend/src/domain/visual-proof-editor/proof-rule/proof-rule-handler/impl-elim-rule-handler.ts
+++ frontend/src/domain/visual-proof-editor/proof-rule/proof-rule-handler/impl-elim-rule-handler.ts
@@ -3,8 +3,21 @@ import { VisualProofEditorRuleHandlerParams, ProofRuleHandlerResult } from '..';
 import { ProofRuleHandler } from './proof-rule-handler';
 import Swal from 'sweetalert2';
 import { createEmptyVisualProofEditorProofTreeFromProp } from '../../../../util/create-visual-proof-editor-empty-proof-tree';
+import { isEqual } from 'lodash';
 
 export class ImplElimRuleHandler extends ProofRuleHandler {
+
+    public getLatexCode(): string {
+        return `
+            \\begin{prooftree}
+                \\AxiomC{$A \\supset B$}
+                \\AxiomC{$A$}
+                \\RightLabel{$\\supset E$}
+                \\BinaryInfC{$B$}
+            \\end{prooftree}
+        `;
+    }
+
     protected async handleRuleUpwards(params: VisualProofEditorRuleHandlerParams): Promise<ProofRuleHandlerResult> {
         const { selectedProofTreeNodes: selecteedProofTreeNodes } = params;
 
@@ -52,10 +65,24 @@ export class ImplElimRuleHandler extends ProofRuleHandler {
     }
 
     protected async handleRuleDownards(params: VisualProofEditorRuleHandlerParams): Promise<ProofRuleHandlerResult> {
+        const selectedNodesLength = params.selectedProofTreeNodes.length;
+
+        if (selectedNodesLength === 1) {
+            return this.handleRuleDownwardsPrincipalConnectiveSelected(params);
+        }
+
+        if (selectedNodesLength === 2) {
+            return this.handleRuleDownwardsBothPremissesSelected(params);
+        }
+
+        throw new Error('Cannot apply rule on this node.');
+    }
+
+    private async handleRuleDownwardsPrincipalConnectiveSelected(params: VisualProofEditorRuleHandlerParams): Promise<ProofRuleHandlerResult> {
         const { selectedProofTreeNodes: selecteedProofTreeNodes } = params;
 
         if (selecteedProofTreeNodes.length !== 1) {
-            throw new Error('Cannot apply rule on this node.');
+            throw new Error('Expected exactly one selected node. This is likely a bug.');
         }
 
         const { proofTree, reasoningContextId } = selecteedProofTreeNodes[0];
@@ -91,6 +118,92 @@ export class ImplElimRuleHandler extends ProofRuleHandler {
                 },
                 nodeId: proofTree.id,
                 reasoningContextId,
+            }],
+        };
+    }
+
+    private async handleRuleDownwardsBothPremissesSelected(params: VisualProofEditorRuleHandlerParams): Promise<ProofRuleHandlerResult> {
+        const { selectedProofTreeNodes: selecteedProofTreeNodes } = params;
+
+        if (selecteedProofTreeNodes.length !== 2) {
+            throw new Error('Expected exactly two selected nodes. This is likely a bug.');
+        }
+
+        const [fst, snd] = selecteedProofTreeNodes;
+
+        const fstConclusion = fst.proofTree.conclusion;
+        const sndConclusion = snd.proofTree.conclusion;
+
+        const fstIsProp = fstConclusion.kind === 'PropIsTrue';
+        const sndIsProp = sndConclusion.kind === 'PropIsTrue';
+
+        if (!fstIsProp || !sndIsProp) {
+            throw new Error('Both premisses have to be proposition judgments.');
+        }
+
+        const fstIsImpl = fstConclusion.value.kind === 'Impl';
+        const sndIsImpl = sndConclusion.value.kind === 'Impl';
+
+        if (!fstIsImpl && !sndIsImpl) {
+            throw new Error('Expected principal connective to be implication.');
+        }
+
+        // find principal connective
+
+        let principal;
+        let applicant;
+        if (fstIsImpl && !sndIsImpl) {
+            principal = fst;
+            applicant = snd;
+        }
+
+        if (!fstIsImpl && sndIsImpl) {
+            principal = snd;
+            applicant = fst;
+        }
+
+        // We cannot use fstIsImpl and sndIsImpl because of Typescript's type system ._.
+        if (fstConclusion.value.kind === 'Impl' && sndConclusion.value.kind === 'Impl') {
+            const fstAntecedent = fstConclusion.value.value[0];
+
+            if (fstAntecedent === sndConclusion.value) {
+                principal = fst;
+                applicant = snd;
+            } else {
+                principal = snd;
+                applicant = fst;
+            }
+        }
+
+        // check for compatibility
+
+        // The first three checks are needed because TypeScript is not smart enough ._.
+
+        if (
+            principal?.proofTree.conclusion.kind !== 'PropIsTrue' ||
+            applicant?.proofTree.conclusion.kind !== 'PropIsTrue' ||
+            principal?.proofTree.conclusion.value.kind !== 'Impl' ||
+            !isEqual(principal?.proofTree.conclusion.value.value![0], applicant?.proofTree.conclusion.value)
+        ) {
+            throw new Error('Premisses are not compatible.');
+        }
+
+        return {
+            additionalAssumptions: [],
+            proofTreeChanges: [],
+            removedReasoingContextIds: [fst.reasoningContextId, snd.reasoningContextId],
+            newReasoningContexts: [{
+                id: v4(),
+                isDragging: false,
+                selectedNodeId: null,
+                x: 0,
+                y: 0,
+                proofTree: {
+                    id: v4(),
+                    premisses: [fst.proofTree, snd.proofTree],
+                    rule: { kind: 'ImplElim' },
+                    conclusion: { kind: 'PropIsTrue', value: principal.proofTree.conclusion.value.value[1] }
+                },
             }],
         };
     }
