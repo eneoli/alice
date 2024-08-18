bbc9a5fab2282329a2ee406e9eb84acb17129424
eneoli <oliver@eneoli.de>
2024-08-18 14:13:38 +0200

    added onReset prop

--- frontend/src/domain/visual-proof-editor/components/visual-proof-editor.tsx
+++ frontend/src/domain/visual-proof-editor/components/visual-proof-editor.tsx
@@ -22,9 +22,10 @@ import { v4 } from 'uuid';
 interface VisualProofEditorProps {
     prop: Prop;
     onProofTreeChange: (tree: VisualProofEditorProofTree) => void;
+    onReset: () => void;
 }
 
-export function VisualProofEditor({ prop, onProofTreeChange }: VisualProofEditorProps) {
+export function VisualProofEditor({ prop, onProofTreeChange, onReset }: VisualProofEditorProps) {
 
     const isControlKeyActive = useKeyPressed('Control');
 
@@ -62,6 +63,7 @@ export function VisualProofEditor({ prop, onProofTreeChange }: VisualProofEditor
         setAssumptions([]);
 
         generateIdentifier.reset();
+        onReset();
     }, [prop]);
 
     useEffect(resetProofEditor, [prop]);
