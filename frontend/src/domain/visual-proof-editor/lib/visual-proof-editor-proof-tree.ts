d079f5174bf9e54639fe4d02fdc72847f7d9c34a
eneoli <oliver@eneoli.de>
2024-08-20 00:09:21 +0200

    added aliceProofTreeIntoVisualProofEditorProofTree

--- frontend/src/domain/visual-proof-editor/lib/visual-proof-editor-proof-tree.ts
+++ frontend/src/domain/visual-proof-editor/lib/visual-proof-editor-proof-tree.ts
@@ -1,5 +1,7 @@
-import { Identifier, ProofTree, ProofTreeConclusion, ProofTreeRule, Prop } from 'alice';
+import { Identifier, instantiate_free_parameter, ProofTree, ProofTreeConclusion, ProofTreeRule, Prop } from 'alice';
 import { v4 } from 'uuid';
+import { AssumptionContext } from '../proof-rule';
+import { flatten } from 'lodash';
 
 export interface VisualProofEditorProofTree {
     id: string,
@@ -14,6 +16,152 @@ export function visualProofEditorProofTreeIntoAliceProofTree(proofTree: VisualPr
         rule: proofTree.rule ?? { kind: 'Sorry' },
         conclusion: proofTree.conclusion,
     }
+}
+
+interface AliceProofTreeIntoVisualProofEditorProofTreeResult {
+    proofTree: VisualProofEditorProofTree;
+    assumptions: AssumptionContext[];
+}
+
+export function aliceProofTreeIntoVisualProofEditorProofTree(reasoningContextId: string, proofTree: ProofTree): AliceProofTreeIntoVisualProofEditorProofTreeResult {
+
+    const premisseResults = proofTree.premisses.map(aliceProofTreeIntoVisualProofEditorProofTree.bind(null, reasoningContextId));
+    const premisseTrees = premisseResults.map((result) => result.proofTree);
+    const premisseAssumptions = flatten(premisseResults.map((result) => result.assumptions));
+
+    const nodeId = v4();
+
+    const visualProofTree: VisualProofEditorProofTree = {
+        id: nodeId,
+        premisses: premisseTrees,
+        rule: proofTree.rule.kind === 'Sorry' ? null : proofTree.rule,
+        conclusion: proofTree.conclusion,
+    };
+
+    const assumptions = premisseAssumptions;
+
+    if (proofTree.rule.kind === 'ImplIntro') {
+
+        if (proofTree.conclusion.kind !== 'PropIsTrue') {
+            throw new Error('Expected conclusion to be a proposition');
+        }
+
+        if (proofTree.conclusion.value.kind !== 'Impl') {
+            throw new Error('Expected conclusion to be an implication');
+        }
+
+        const prop = proofTree.conclusion.value.value[0];
+
+        assumptions.push({
+            owningReasoningCtxId: reasoningContextId,
+            owningNodeId: nodeId,
+            assumption: {
+                kind: 'PropIsTrue',
+                ident: proofTree.rule.value,
+                prop,
+            }
+        });
+    }
+
+    if (proofTree.rule.kind === 'ForAllIntro') {
+
+        if (proofTree.conclusion.kind !== 'PropIsTrue') {
+            throw new Error('Expected conclusion to be a proposition.');
+        }
+
+        if (proofTree.conclusion.value.kind !== 'ForAll') {
+            throw new Error('Expected conclsuion to be universal quantification.');
+        }
+
+        const datatype = proofTree.conclusion.value.value.object_type_ident;
+
+        assumptions.push({
+            owningReasoningCtxId: reasoningContextId,
+            owningNodeId: nodeId,
+            assumption: {
+                kind: 'Datatype',
+                ident: proofTree.rule.value,
+                datatype,
+            }
+        });
+    }
+
+    if (proofTree.rule.kind === 'OrElim') {
+
+        if (proofTree.conclusion.kind !== 'PropIsTrue') {
+            throw new Error('Expected conclusion to be a proposition.');
+        }
+
+        if (proofTree.conclusion.value.kind !== 'Or') {
+            throw new Error('Expected conclusion to be a disjunction.');
+        }
+
+        const [fst, snd] = proofTree.conclusion.value.value;
+
+        assumptions.push({
+            owningReasoningCtxId: reasoningContextId,
+            owningNodeId: nodeId,
+            assumption: {
+                kind: 'PropIsTrue',
+                ident: proofTree.rule.value[0],
+                prop: fst,
+            }
+        });
+
+        assumptions.push({
+            owningReasoningCtxId: reasoningContextId,
+            owningNodeId: nodeId,
+            assumption: {
+                kind: 'PropIsTrue',
+                ident: proofTree.rule.value[1],
+                prop: snd,
+            },
+        });
+    }
+
+    if (proofTree.rule.kind === 'ExistsElim') {
+
+        if (proofTree.premisses[0].conclusion.kind !== 'PropIsTrue') {
+            throw new Error('Expected first premisse to be a proposition.');
+        }
+
+        if (proofTree.premisses[0].conclusion.value.kind !== 'Exists') {
+            throw new Error('Expected first premisse to be an existential quantification.');
+        }
+
+        const datatype = proofTree.premisses[0].conclusion.value.value.object_type_ident;
+
+        const binding_ident = proofTree.premisses[0].conclusion.value.value.object_ident;
+
+        const uninstantiated_body = proofTree.premisses[0].conclusion.value.value.body;
+
+        const instantiated_body = instantiate_free_parameter(uninstantiated_body, binding_ident, proofTree.rule.value[0]);
+
+        assumptions.push({
+            owningReasoningCtxId: reasoningContextId,
+            owningNodeId: nodeId,
+            assumption: {
+                kind: 'Datatype',
+                ident: proofTree.rule.value[0],
+                datatype,
+            }
+        });
+
+        assumptions.push({
+            owningReasoningCtxId: reasoningContextId,
+            owningNodeId: nodeId,
+            assumption: {
+                kind: 'PropIsTrue',
+                ident: proofTree.rule.value[1],
+                prop: instantiated_body,
+            }
+        });
+    }
+
+    return {
+        proofTree: visualProofTree,
+        assumptions,
+    };
 }
 
 export function createEmptyVisualProofEditorProofTreeFromConclusion(conclusion: ProofTreeConclusion) {
