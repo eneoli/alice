a4ddb3caebf95f50de21c4818399eb2496541b8c
eneoli <oliver@eneoli.de>
2024-08-19 21:54:03 +0200

    fixed print for datatype

--- frontend/src/util/print-type.ts
+++ frontend/src/util/print-type.ts
@@ -3,6 +3,6 @@ import { print_prop, Type } from 'alice';
 export function printType(type: Type) {
     switch (type.kind) {
         case 'Prop': return print_prop(type.value);
-        case 'Datatype': return `${type.value[0]}: ${type.value[1]}`;
+        case 'Datatype': return `${type.value}`;
     }
 }
