49ad9768931d07f67d6dee83ba2c1b84a0be6971
Oliver Enes <oliver@eneoli.de>
2024-09-05 05:30:17 +0200

    cleanup

--- deploy.sh
+++ deploy.sh
@@ -31,8 +31,12 @@ echo "$SSH_KEY" > ~/.ssh/deploy.key
 sudo chmod 600 ~/.ssh/deploy.key
 echo "$SSH_KNOWN_HOSTS" > ~/.ssh/known_hosts
 
+rm -rf dist.zip
+zip -r dist.zip dist/
+
 ssh -i ~/.ssh/deploy.key $DEPLOYMENT_USER@$DEPLOYMENT_SERVER "rm -rf $DEPLOYMENT_RES_PATH/*"
-scp -i ~/.ssh/deploy.key -r dist $DEPLOYMENT_USER@$DEPLOYMENT_SERVER:$DEPLOYMENT_RES_PATH
+scp -i ~/.ssh/deploy.key dist.zip $DEPLOYMENT_USER@$DEPLOYMENT_SERVER:$DEPLOYMENT_RES_PATH
+ssh -i ~/.ssh/deploy.key $DEPLOYMENT_USER@$DEPLOYMENT_SERVER "unzip $DEPLOYMENT_RES_PATH/dist.zip && mv dist/* ../ && rm -rf dist/ && rm -rf dist.zip"
 
 echo -e "\033[1;32m"
 echo "Done ^-^"
